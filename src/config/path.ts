8cd25afc10e751f8d1c2923773af6ee77f598ed1
Dima Lipski <lipsdima@yandex.ru>
2022-11-30 21:21:03 +0300

    config

---
+++ src/config/path.ts
@@ -0,0 +1,8 @@
+export enum Path  {
+    HOME = "/",
+    TRENDS = "/trends",
+    FAVORITES = "/favorites",
+    SETTINGS = "/settings",
+    LOGIN = "/login",
+    REGISTRATION = "/registration",
+}
