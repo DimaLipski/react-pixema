5de364497194189a8a3f4df78ac7e93fe5f5f91d
Dima Lipski <lipsdima@yandex.ru>
2022-12-03 23:37:19 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './components/AppRouter/AppRouter';
+import { MenuNav } from './components/MenuNav/MenuNav';
+import { AppWrapper } from './ui';
+
+type Theme = "dark" | "light";
 
 export const App = () => {
+  const [theme, setTheme] = useState<Theme>("dark");
+  useEffect(() => document.documentElement.setAttribute("theme", theme), [theme]);
+
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <AppWrapper>
+        <MenuNav />
+        <AppRouter />
+      </AppWrapper>
+    </BrowserRouter>
+  );
+};
