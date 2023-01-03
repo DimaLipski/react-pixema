5c7dd25b001c85d28fbbb89ea8ed82f55d5b1a08
Dima Lipski <lipsdima@yandex.ru>
2023-01-03 23:02:22 +0300

    feat(components): add favoritesList

--- src/components/FavoritesList/FavoritesList.tsx
+++ src/components/FavoritesList/FavoritesList.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useAppSelector } from "../../store/hooks/hooks";
+import { getFavorites } from "../../store/selectors/favoritesSelector";
+import { IMovie } from "../../types/types";
+import { MovieListItem } from "../MovieListItem/MovieListItem";
+import { StyledFavoritesList } from "./styles";
 
 export const FavoritesList = () => {
+  const { favorites } = useAppSelector(getFavorites);
+
   return (
-    <div>FavoritesList</div>
-  )
-}
+    <StyledFavoritesList>
+      {favorites.map(({ year, title, poster, imdbID }: IMovie) => (
+        <MovieListItem
+          key={imdbID}
+          year={year}
+          title={title}
+          poster={poster}
+          imdbID={imdbID}
+          isFavorite={true}
+        />
+      ))}
+    </StyledFavoritesList>
+  );
+};
