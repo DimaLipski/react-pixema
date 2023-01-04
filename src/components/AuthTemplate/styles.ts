287b6543652c82a6932d7f270acb58fd59a5e2fd
Dima Lipski <lipsdima@yandex.ru>
2023-01-04 16:53:20 +0300

    fix(components): spaces removed

--- src/components/AuthTemplate/styles.ts
+++ src/components/AuthTemplate/styles.ts
@@ -1,6 +1,5 @@
-
+import { backgroundAuth } from "assets";
 import styled from "styled-components";
-import { backgroundAuth } from "../../assets";
 
 export const Wrapper = styled.div`
   height: 100vh;
