22975020dd96b71b57f7a2ab84685f489e19ae4e
Dima Lipski <lipsdima@yandex.ru>
2023-01-04 16:54:49 +0300

    fix(components): bugs fixed

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,8 +1,9 @@
-import { ToolBar, Title, Wrapper, CloseButton } from "./styles";
-import { AnimatePresence } from "framer-motion";
 import { CustomLink } from "components";
+import { ToolBar, Title, Wrapper, CloseButton } from "./styles";
+import { HomeIcon, FavoritesIcon, SettingsIcon, TrendsIcon, CloseIcons } from "assets/index";
 import { ROUTE } from "routes";
-
+import { Color } from "ui";
+import { AnimatePresence } from "framer-motion";
 
 interface IProps {
   setToggle: () => void;
@@ -20,22 +21,22 @@ export const BurgerMenu = ({ setToggle, toggle }: IProps) => {
           exit={{ x: 550 }}
         >
           <CloseButton onClick={() => setToggle()}>
-
+            <CloseIcons stroke={Color.White} />
           </CloseButton>
           <ToolBar>
             <CustomLink onClick={() => setToggle()} to={ROUTE.HOME}>
-
+              <HomeIcon width={24} /> <Title>Home</Title>
             </CustomLink>
             <CustomLink onClick={() => setToggle()} to={ROUTE.TRENDS}>
-
+              <TrendsIcon width={24} />
               <Title>Trends</Title>
             </CustomLink>
             <CustomLink onClick={() => setToggle()} to={ROUTE.FAVORITES}>
-
+              <FavoritesIcon width={24} />
               <Title>Favorites</Title>
             </CustomLink>
             <CustomLink onClick={() => setToggle()} to={ROUTE.SETTINGS}>
-
+              <SettingsIcon width={24} />
               <Title>Settings</Title>
             </CustomLink>
           </ToolBar>
--- src/components/BurgerMenu/styles.ts
+++ src/components/BurgerMenu/styles.ts
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Color } from "ui";
 
-
 export const Wrapper = styled(motion.div)`
   position: absolute;
   top: 0;
