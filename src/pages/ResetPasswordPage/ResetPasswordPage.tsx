126dbb02e21fd14a1ffa8bce0722e5141b60d2d7
Dima Lipski <lipsdima@yandex.ru>
2023-01-07 14:35:57 +0300

    feat(components): add resetPassword

--- src/pages/ResetPasswordPage/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import { ResetPassword } from "components";
 
 export const ResetPasswordPage = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  return <ResetPassword />;
+};
