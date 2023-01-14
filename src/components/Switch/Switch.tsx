c9576eeb6142c60263a581ae2777ea2a80e5d038
Dima Lipski <lipsdima@yandex.ru>
2023-01-14 13:59:51 +0300

    feat(components): add switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Button, Circle } from "./styles";
+import { useAppDispatch, useAppSelector } from "store/hooks";
+import { getUserInfo } from "store/selectors";
+import { useLayoutEffect } from "react";
+import { setDarkMode } from "store/features";
 
 export const Switch = () => {
+  const dispatch = useAppDispatch();
+  const { isDark } = useAppSelector(getUserInfo);
+
+  useLayoutEffect(() => {
+    document.documentElement.setAttribute("data-theme", isDark);
+  }, [isDark]);
+
   return (
-    <div>Switch</div>
-  )
-}
+    <Button
+      type="button"
+      onClick={(e) => {
+        e.preventDefault();
+        dispatch(setDarkMode());
+      }}
+      $isDark={isDark}
+    >
+      <Circle $isDark={isDark}></Circle>
+    </Button>
+  );
+};
